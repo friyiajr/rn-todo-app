9777f92ad378b8d7ce2ceaf23bbc723f34aa3276
Daniel Friyia <dan@trainwithpush.com>
2020-10-08 19:45:58 -0400

    Navigation works

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ import Home from './src/Home/Home.screen'
 import { Provider } from 'react-redux';
 import { Navigation } from "react-native-navigation";
 import configureStore from './src/Store/';
+import { prop } from 'ramda';
 
 const store = configureStore();
 
 Navigation.registerComponent(
   'com.myApp.HomeScreen',
-  () => {
-    return () => (
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    );
-  },
+  () => props => (
+    <Provider store={store}>
+      <Home {...props} />
+    </Provider>
+  ),
+  () => Home
 );
+
+// return () => {
+//   return (
+//     <Provider store={store}>
+//       <Home />
+//     </Provider>
+//   )
+// }
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
